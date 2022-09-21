8867796bb06b3237712e3832a7507a3c20605fb9
AAli <ajaouhari75@gmail.com>
2022-09-21 15:42:54 +0200

    Add project files.

---
+++ ManejoPresupuesto/Models/IndiceCuentasViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ManejoPresupuesto.Models
+{
+    public class IndiceCuentasViewModel
+    {
+        public string TipoCuenta { get; set; }
+        public IEnumerable<Cuenta> Cuentas { get; set; }
+
+        //va a ser la suma de todas las cuentas de ese tipo de cuentas
+        public decimal Balance => Cuentas.Sum(x => x.Balance);
+    }
+}
